bc9d61b367842fb64b378c6f016604a76bd02a6a
Garv Jain <garvj03@gmail.com>
2021-11-26 23:59:32 +0530

    chore: test page

--- resources/js/Pages/Index.tsx
+++ resources/js/Pages/Index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Test = () => <div>hello from inertia</div>
+const Test = () => <div className="font-bold font-lg">Sexy Index Page</div>
 
 // All pages need to be exported as default
 export default Test
