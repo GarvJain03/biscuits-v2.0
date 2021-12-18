97d04c1140596b0b1895fe993a32b4b89a95397e
Garv Jain <garvj03@gmail.com>
2021-12-18 23:18:38 +0530

    ui: color pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@ module.exports = {
   purge: ['./resources/**/*.edge', './resources/**/*.js', './resources/**/*.tsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-bg': '#121212',
+        'heading-green': '#1DB954',
+        'text-green': '#1ED760',
+        'button-gray': '#272727',
+        'cool-gray': '#3D3C3C',
+        'gray-light': '#202020',
+      },
+      fontFamily: {
+        sans: ['Chakra Petch', 'sans-serif'],
+      },
+    },
   },
   variants: {
     extend: {},
